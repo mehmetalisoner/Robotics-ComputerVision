8677b1f8b2f41d420d4d3b5def6ec6e082dfba0a
Mehmet Ali Soner <sonermehmetali@gmail.com>
2022-10-18 00:05:15 -0400

    updated test2.py - works

--- HW3/test2.py
+++ HW3/test2.py
@@ -4,9 +4,11 @@ import glob
 import os
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+a = 7
+b = 7
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((5*5,3), np.float32)
-objp[:,:2] = np.mgrid[0:5,0:5].T.reshape(-1,2)
+objp = np.zeros((a*b,3), np.float32)
+objp[:,:2] = np.mgrid[0:a,0:b].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -33,15 +35,16 @@ for filename in os.listdir(folder):
     cv2.waitKey(3)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (5,5), None)
+    ret, corners = cv2.findChessboardCorners(gray, (a,b), None)
+    print(ret,corners)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
+        found += 1 
         objpoints.append(objp)
-        corners2 = cv2.cornerSubPix(gray,corners, criteria)
-        imgpoints.append(corners2)
+        corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (5,5), corners2, ret)
+        cv2.drawChessboardCorners(img, (a,b), corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(500)
 
