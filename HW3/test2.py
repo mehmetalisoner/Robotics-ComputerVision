37bb3a159ddbeba27c4959b41dff8b9ff783d08d
Mehmet Ali Soner <sonermehmetali@gmail.com>
2022-10-17 23:56:19 -0400

    updated test2.py

--- HW3/test2.py
+++ HW3/test2.py
@@ -5,8 +5,8 @@ import os
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((5*5,3), np.float32)
+objp[:,:2] = np.mgrid[0:5,0:5].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -18,6 +18,7 @@ size = 0
 images = []
 for filename in os.listdir(folder):
     path = os.path.join(folder,filename)                    # create path for image
+    print(path)
     img = cv2.imread(path)                                  # read image
     
     # Downscale image because photos shot on iPhone are too big
@@ -28,17 +29,19 @@ for filename in os.listdir(folder):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)            # convert to gray image
     
     cv2.imshow('img',gray)
-    cv2.waitKey(500)
+    #cv2.waitKey(300)
+    cv2.waitKey(3)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (5,5), None)
+
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-        corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        corners2 = cv2.cornerSubPix(gray,corners, criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv2.drawChessboardCorners(img, (5,5), corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(500)
 
