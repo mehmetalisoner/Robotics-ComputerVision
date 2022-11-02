0b76743fe9794e3017a779acc91937e28485af3e
Mehmet Ali Soner <sonermehmetali@gmail.com>
2022-11-02 18:59:16 -0400

    updated test5.py

--- Project1/test5.py
+++ Project1/test5.py
@@ -1,15 +1,21 @@
 # Example from https://pytorch.org/hub/pytorch_vision_resnet/
 
-# WORKS and PRETTY GOOD
+# WORKS and PRETTY GOOD, multiple images
 
 import torch
 import os
 from PIL import Image
 from torchvision import transforms
-model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+import urllib
+
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
 model.eval()
 
-import urllib
+
 # Try with image from URL
 # url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
 # try: urllib.URLopener().retrieve(url, filename)
@@ -25,8 +31,10 @@ preprocess = transforms.Compose([
 
 # Test with images in folder
 folder = "test1"
-#filename = "l3.jpg"
-#filename  = os.path.join(folder,filename)
+y_pred = []
+y_true = []
+
+
 for filename in os.listdir(folder):
     filename = os.path.join(folder,filename)                    
     input_image = Image.open(filename) 
@@ -40,11 +48,8 @@ for filename in os.listdir(folder):
 
     with torch.no_grad():
         output = model(input_batch)
-    # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-    # print(output[0])
-    # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+    
     probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    # print(probabilities)
 
 
     # Read the categories
@@ -55,7 +60,34 @@ for filename in os.listdir(folder):
     for i in range(top5_prob.size(0)):
         print(categories[top5_catid[i]], top5_prob[i].item())
     print("New item:")
+    y_pred.append(categories[top5_catid[0]]) # Save Prediction
+    #y_true.extend()
+
+
+print(y_pred)
 
 
 
 
+
+# # iterate over test data
+# for inputs, labels in testloader:
+#         output = net(inputs) # Feed Network
+
+#         output = (torch.max(torch.exp(output), 1)[1]).data.cpu().numpy()
+#         y_pred.extend(output) # Save Prediction
+        
+#         labels = labels.data.cpu().numpy()
+#         y_true.extend(labels) # Save Truth
+
+# # constant for classes
+# classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+#         'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+
+# # Build confusion matrix
+# cf_matrix = confusion_matrix(y_true, y_pred)
+# df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix) *10, index = [i for i in classes],
+#                      columns = [i for i in classes])
+# plt.figure(figsize = (12,7))
+# sn.heatmap(df_cm, annot=True)
+# plt.savefig('output.png')
