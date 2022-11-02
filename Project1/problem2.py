2cd51fd4889147d499c5fcfe508288c77d79ead3
Mehmet Ali Soner <sonermehmetali@gmail.com>
2022-11-02 18:42:56 -0400

    created problem2.py

---
+++ Project1/problem2.py
@@ -0,0 +1,14 @@
+# Packages
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import os
+from torchvision import transforms
+import pandas as pd
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
