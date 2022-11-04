8197ef5f09e81441da68cacbbd17fe59506ad6cf
Mehmet Ali Soner <sonermehmetali@gmail.com>
2022-11-04 16:07:56 -0400

    updated problem1.py: finished

--- Project1/problem1.py
+++ Project1/problem1.py
@@ -1,122 +1,94 @@
-# Packages
+# Example from https://pytorch.org/hub/pytorch_vision_resnet/
+
+# WORKS and PRETTY GOOD, multiple images
+
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
 import os
+from PIL import Image
 from torchvision import transforms
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
 import pandas as pd
-from torchvision.io import read_image
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import urllib
+import cv2
 import matplotlib.pyplot as plt
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# transform = transforms.Compose([            
-#     transforms.Resize(256),                    
-#     transforms.CenterCrop(224),                
-#     transforms.ToTensor(),                     
-#     transforms.Normalize(                      
-#     mean=[0.485, 0.456, 0.406],                
-#     std=[0.229, 0.224, 0.225]                  
-# )])
+import numpy as np
 
 
 
+# Init device
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f'Using {device} for inference')
 
-# learning_rate = 1e-3
-# num_epochs = 10
-# batch_size = 2
-# num_classes = 5
-
-
-
-# class CustomImageDataset(Dataset):
-#     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-#         self.img_labels = pd.read_csv(annotations_file)
-#         self.img_dir = img_dir
-#         self.transform = transform
-#         self.target_transform = target_transform
-
-#     def __len__(self):
-#         return len(self.img_labels)
-
-#     def __getitem__(self, idx):
-#         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-#         image = read_image(img_path)
-#         label = self.img_labels.iloc[idx, 1]
-#         if self.transform:
-#             image = self.transform(image)
-#         if self.target_transform:
-#             label = self.target_transform(label)
-#         return image, label
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+model = model.to(device)
+model.eval()
 
 
 
 
-# train_data = CustomImageDataset(csv_file = 'a.csv', root_dir = 'train1',transform=transform)
-# test_data = CustomImageDataset(csv_file = 'a.csv', root_dir = 'test1',transform=transform)
-# train_loader = DataLoader(train_data,batch_size=batch_size,shuffle=True)
-# test_loader = DataLoader(test_data,batch_size=batch_size,shuffle=True)
+# Transform input images
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-# model = torchvision.models.resnet50(preTrained=True)
-# model.to(device)
+# Folder names
+test_folder = "dataset"
+project_folder = os.getcwd()
+test_folder = os.path.join(project_folder,test_folder)
 
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-# for epoch in range(num_epochs):
-#     losses = []
-
-#     for batch_idx, (data, targets) in enumerate(train_loader):
-#         # Get data to cuda if possible
-#         data = data.to(device=device)
-#         targets = targets.to(device=device)
-
-#         # forward
-#         scores = model(data)
-#         loss = criterion(scores, targets)
-
-#         losses.append(loss.item())
-
-#         # backward
-#         optimizer.zero_grad()
-#         loss.backward()
-
-#         # gradient descent or adam step
-#         optimizer.step()
-
-#     print(f"Cost at epoch {epoch} is {sum(losses) / len(losses)}")
+# Arrays for predictions and actual values
+y_pred = []
+y_true = []
 
 
-# # Check accuracy on training to see how good our model is
-# def check_accuracy(loader, model):
-#     num_correct = 0
-#     num_samples = 0
-#     model.eval()
+for folder in os.listdir(test_folder):
+    sub_directory = (os.path.join(test_folder,folder))
+    if (os.path.isdir(sub_directory) == False): continue
+    # print(sub_directory)
+    for filename in os.listdir(sub_directory):
+        print(filename)
+        label = folder
+        filename_path = os.path.join(sub_directory,filename)
 
-#     with torch.no_grad():
-#         for x, y in loader:
-#             x = x.to(device=device)
-#             y = y.to(device=device)
+        # Evaluation
+        original_img = Image.open(filename_path)
+        input_tensor = preprocess(original_img)
+        input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+        input_batch = input_batch.to(device)    # move batch to device
 
-#             scores = model(x)
-#             _, predictions = scores.max(1)
-#             num_correct += (predictions == y).sum()
-#             num_samples += predictions.size(0)
+        with torch.no_grad():
+            output = model(input_batch)
+        
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
-#         print(
-#             f"Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}"
-#         )
+        # Read the categories
+        with open("imagenet_classes.txt", "r") as f:
+            categories = [s.strip() for s in f.readlines()]
+        # Show top categories per image
+        top5_prob, top5_catid = torch.topk(probabilities, 5)
+        for i in range(top5_prob.size(0)):
+            cv2.imshow('Original image', cv2.imread(filename_path))
+            cv2.waitKey(500)
+            print(categories[top5_catid[i]], top5_prob[i].item())
+        print("\n")
+        y_pred.append(categories[top5_catid[0]]) # Save Prediction
+        y_true.append(label)
 
-#     model.train()
 
+print(y_pred)
+print(y_true)
 
-# print("Checking accuracy on Training Set")
-# check_accuracy(train_loader, model)
+# constant for classes
+classes = ('balloon', 'Labrador retriever', 'barbell','ski', 'velvet')
 
-# print("Checking accuracy on Test Set")
-# check_accuracy(test_loader, model)
+# Build confusion matrix
+cf_matrix = confusion_matrix(y_true, y_pred)
+df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix) *5, index = [i for i in classes],
+                     columns = [i for i in classes])
+plt.figure(figsize = (12,7))
+sn.heatmap(df_cm, annot=True)
+plt.show()
