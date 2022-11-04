8654b3e4394f66b87d7763401ab9bf5bd97912d6
Mehmet Ali Soner <sonermehmetali@gmail.com>
2022-11-04 09:19:37 -0400

    updated noistest.py

--- Project1/noisetest.py
+++ Project1/noisetest.py
@@ -38,24 +38,26 @@ for folder in os.listdir(test_folder):
     for filename in os.listdir(sub_directory):
         label = folder
         filename_path = os.path.join(sub_directory,filename)
-        print(filename_path)
-        original_img = cv2.imread(filename_path) 
+        original_img = cv2.imread(filename_path)
+        cv2.imshow('Original', original_img) 
+        cv2.waitKey(3000)
         # Generate Gaussian noise
         gauss = np.random.normal(0,1,original_img.size)
         gauss = gauss.reshape(original_img.shape[0],original_img.shape[1],original_img.shape[2]).astype('uint8')
         # Add the Gaussian noise to the image
         img_gauss = cv2.add(original_img,gauss)
         # Display the image
-        # cv2.imshow('a',img_gauss)
-        # cv2.waitKey(0)
-        os.chdir(noisy_folder)
-        cv2.imwrite("noisy_"+filename,img_gauss)
+        cv2.imshow('Noisy',img_gauss)
+        cv2.waitKey(3000)
+        cv2.destroyAllWindows()
+        # os.chdir(noisy_folder)
+        # cv2.imwrite("noisy_"+filename,img_gauss)
 
 
-# os.chdir(project_folder)
-for image in os.listdir(noisy_folder):
-    image_path = os.path.join(noisy_folder,image)
-    img = cv2.imread(image_path)
-    print(image_path)
-    cv2.imshow('a',img)
-    cv2.waitKey(0)
+# # os.chdir(project_folder)
+# for image in os.listdir(noisy_folder):
+#     image_path = os.path.join(noisy_folder,image)
+#     img = cv2.imread(image_path)
+#     print(image_path)
+#     cv2.imshow('a',img)
+#     cv2.waitKey(3)
